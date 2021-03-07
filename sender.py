19a361f64bd2f95eb0999589bf5aeecfc2754b5b
janelletait <65558755+janelletait@users.noreply.github.com>
2021-03-07 16:22:01 -0500

    Update sender.py

--- sender.py
+++ sender.py
@@ -100,7 +100,7 @@ class sender:
     
     
         if((self.isCorrupted(packet) != TRUE) or (self.isDuplicate(packet) != TRUE)):
-            self.networkSimulator.stopTimer(self.networkSimulator, self.entity)
+            self.networkSimulator.stopTimer(self.entity)
             self.SeqNum = self.getNextSeqNum(self)
             
             
